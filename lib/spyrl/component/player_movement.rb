3729d56071dfe77d7add58065df27eb5cadd5507
BennyHallett <benny.hallett@gmail.com>
2014-03-11 08:39:22 +1100

    Added components for player and enemy movement

---
+++ lib/spyrl/component/player_movement.rb
@@ -0,0 +1,56 @@
+class RandomMovementComponent
+  def initialize(parent)
+    raise 'Cannot create walkable component when parent is nil' unless parent
+
+    @parent = parent
+  end
+
+  def id
+    :actor
+  end
+
+  def act
+    char = input.wait_for_input
+    move = @player.get(:movement)
+
+    move.north if char == 'k'
+    move.south if char == 'j'
+    move.east if char == 'l'
+    move.west if char == 'h'
+    move.north_east if char == 'u'
+    move.north_west if char == 'y'
+    move.south_east if char == 'n'
+    move.south_west if char == 'b'
+
+    if char == 'o'
+      open_dir = input.wait_for_input
+      open = @player.get(:open)
+
+      open.north if open_dir == 'k'
+      open.south if open_dir == 'j'
+      open.east if open_dir == 'l'
+      open.west if open_dir == 'h'
+      open.north_east if open_dir == 'u'
+      open.north_west if open_dir == 'y'
+      open.south_east if open_dir == 'n'
+      open.south_west if open_dir == 'b'
+    end
+
+   if char == 'c'
+      close_dir = input.wait_for_input
+      close = @player.get(:close)
+
+      close.north if close_dir == 'k'
+      close.south if close_dir == 'j'
+      close.east if close_dir == 'l'
+      close.west if close_dir == 'h'
+      close.north_east if close_dir == 'u'
+      close.north_west if close_dir == 'y'
+      close.south_east if close_dir == 'n'
+      close.south_west if close_dir == 'b'
+    end
+
+    char == 'x'
+  end
+
+end
---
+++ lib/spyrl/component/random_movement.rb
@@ -0,0 +1,26 @@
+class RandomMovementComponent
+  def initialize(parent)
+    raise 'Cannot create walkable component when parent is nil' unless parent
+
+    @parent = parent
+  end
+
+  def id
+    :actor
+  end
+
+  def act
+    if @parent.has? :movemment
+      dir = (rand * 4).floor
+      move = @parent.get :movement
+
+      move.north if dir == 0
+      move.east if dir == 1
+      move.south if dir == 2
+      move.west if dir == 3
+    end
+
+    false
+  end
+
+end
